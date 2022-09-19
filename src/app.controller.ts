e6ba66e86b700abbb66c6f50cac0100aa714ca9e
Donatus Brain Okwe <braindona@gmail.com>
2022-09-19 06:46:50 -0500

    Completed the delete handler

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Put, Delete, Param, Body} from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, Param, Body, HttpCode} from '@nestjs/common';
 import { AppService } from './app.service';
 import { data, IData, ReportType } from 'src/data';
 import { v4 as uuid } from 'uuid'
@@ -56,9 +56,14 @@ export class AppController {
     return data.report[reportIndex]
   }
 
+  @HttpCode(204)
   @Delete('/:id')
-  deleteReport() {
-    return 'Report Deleted';
+  deleteReport(@Param() id: string) {
+    const reportIndex = data.report.findIndex(report => report.id === id)
+    if(reportIndex === -1) return
+
+    data.report.splice(reportIndex, 1);
+    return;
   }
 
   @Get()
