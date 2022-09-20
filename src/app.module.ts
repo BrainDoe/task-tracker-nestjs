09a972c28c0b6c07410d660486c482f48d565868
Donatus Brain Okwe <braindona@gmail.com>
2022-09-20 05:04:18 -0500

    Worked on the summary module. Injected the ReportModule into the SummaryModule (Dependency Injection In Practice). Concluded the project by created a handler to calculate the summary of the income and expense

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,14 +3,15 @@ import { APP_INTERCEPTOR } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ReportModule } from 'src/report/report.module';
+import { SummaryModule } from 'src/summary/summary.module';
 import { CustomInterceptor } from './interceptors/custom.interceptor'
 
 @Module({
-  imports: [ReportModule],
+  imports: [ReportModule, SummaryModule],
   controllers: [AppController],
   providers: [AppService, {
     provide: APP_INTERCEPTOR,
     useClass: ClassSerializerInterceptor
-  }],
+  }]
 })
 export class AppModule {}
